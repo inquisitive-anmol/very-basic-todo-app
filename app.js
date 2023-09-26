7881071b2d3e2b5b672e3c6a56335900d4abd455
❤anmol❤ <131289076+inquisitive-anmol@users.noreply.github.com>
2023-09-26 23:38:51 +0530

    This is the javascript file

---
+++ app.js
@@ -0,0 +1,35 @@
+let todo = [];
+
+let req = prompt("Enter your request")
+
+while(true){
+    if(req == "quit"){
+        console.log("quitting app...");
+        console.log("app quitted")
+        break;
+
+    }
+    else if(req == "list"){
+        console.log("-------------");
+        for(let i = 0; i<todo.length; i++){
+            console.log(i, todo[i]);
+
+        }
+        console.log("-------------");
+    } else if (req == "add"){
+        let task = prompt("Enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+
+    } else if (req == "delete"){
+        let idx = prompt("Enter the index of task you want to delete");
+        todo.splice(idx, 1);
+        console.log("task deleted");
+
+    }
+    else{
+        console.log("wrong request");
+    }
+
+    req = prompt("Enter your request");
+}
